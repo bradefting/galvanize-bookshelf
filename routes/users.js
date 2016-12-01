db818fa96540e268fc852f6d2c445ebe43b4f2d4
Brad Efting <bradefting@gmail.com>
2016-12-01 15:43:33 -0700

    pt3 tests pass second time bc tests id:2

--- routes/users.js
+++ routes/users.js
@@ -5,24 +5,28 @@ const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 const knex = require('../knex');
+const bcrypt = require('bcrypt-as-promised');
 const { camelizeKeys, decamelizeKeys } = require('humps');
 
 router.post('/users', (req, res, next) => {
-
- knex('users')
-     .insert({
-       first_name: req.body.first_name,
-       last_name: req.body.last_name,
-       email: req.body.email,
-       hashed_password: req.body.password
-     }, '*')
-   .then((userSnakeCase) => {
-     var user = camelizeKeys(userSnakeCase);
-     res.send(user[0]);
-   })
-   .catch((err) => {
-     next(err);
-   });
+  bcrypt.hash(req.body.password, 8)
+    .then((hashed_password) => {
+         return knex('users')
+          .insert({
+            first_name: req.body.firstName,
+            last_name: req.body.lastName,
+            email: req.body.email,
+            hashed_password: hashed_password
+          }, '*');
+      })
+     .then((userSnake) => {
+       const user = camelizeKeys(userSnake);
+       delete user[0].hashedPassword;
+       res.send(user[0]);
+     })
+     .catch((err) => {
+       next(err);
+     });
 });
 
 module.exports = router
